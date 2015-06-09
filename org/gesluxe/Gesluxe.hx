79b893ec70c254e3351462c6d2e6afb82ec56f83
jigoa <josuigoa@gmail.com>
2015-06-09 12:10:13 +0200

    removed condition where the touchevent fires only in mobile devices. now touch events even work in web target (mobile browsers)

--- org/gesluxe/Gesluxe.hx
+++ org/gesluxe/Gesluxe.hx
@@ -16,30 +16,29 @@ class Gesluxe
 	static inline var MOUSE_TOUCH_POINT_ID:UInt = 0;
 	static var touchesManager(null, set):TouchesManager;
 	static public function set_touchesManager(tm:TouchesManager):TouchesManager { touchesManager = tm; return tm; }
-	
+
 	static public var gesturesManager:GesturesManager;
 	static var _touchesManager:TouchesManager;
-	
-	public function new() 
+
+	public function new()
 	{
 	}
-	
-	static public function init() 
+
+	static public function init()
 	{
 		GestureState.initStates();
 		gesturesManager = new GesturesManager();
 		_touchesManager = new TouchesManager(gesturesManager);
-		
-		#if !mobile
+
+		// desktop and desktop web browsers
 		Luxe.core.emitter.on(Luxe.Ev.mousedown, onmousedown);
-		#else
+		// mobile and mobile web browsers
 		Luxe.core.emitter.on(Luxe.Ev.touchdown, ontouchdown);
 		Luxe.core.emitter.on(Luxe.Ev.touchmove, ontouchmove);
 		Luxe.core.emitter.on(Luxe.Ev.touchup, ontouchup);
-		#end
 	}
-	
-	static function ontouchdown(event:TouchEvent) 
+
+	static function ontouchdown(event:TouchEvent)
 	{
 		/*
 		 * state : InteractState.down,
@@ -52,61 +51,60 @@ class Gesluxe
 			pos : _touch_pos*/
 		_touchesManager.onTouchBegin(event.touch_id, event.pos.x * Luxe.screen.w, event.pos.y * Luxe.screen.h); //, event.target as InteractiveObject);
 	}
-	
-	static function ontouchmove(event:TouchEvent) 
+
+	static function ontouchmove(event:TouchEvent)
 	{
 		_touchesManager.onTouchMove(event.touch_id, event.pos.x * Luxe.screen.w, event.pos.y * Luxe.screen.h);
 	}
-	
-	static function ontouchup(event:TouchEvent) 
+
+	static function ontouchup(event:TouchEvent)
 	{
 		_touchesManager.onTouchEnd(event.touch_id, event.pos.x * Luxe.screen.w, event.pos.y * Luxe.screen.h);
 	}
-	
+
 	static function onmousedown(event:MouseEvent)
 	{
 		var touchAccepted:Bool = _touchesManager.onTouchBegin(MOUSE_TOUCH_POINT_ID, event.pos.x, event.pos.y);
-			
+
 		if (touchAccepted)
 			addmouselisteners();
 	}
-	
-	static private function onmousemove(event:MouseEvent) 
+
+	static private function onmousemove(event:MouseEvent)
 	{
 		_touchesManager.onTouchMove(MOUSE_TOUCH_POINT_ID, event.pos.x, event.pos.y);
 	}
-	
-	static private function onmouseup(event:MouseEvent) 
+
+	static private function onmouseup(event:MouseEvent)
 	{
 		_touchesManager.onTouchEnd(MOUSE_TOUCH_POINT_ID, event.pos.x, event.pos.y);
-			
+
 		if (_touchesManager.activeTouchesCount == 0)
 			removemouselisteners();
 	}
-	
+
 	static function addmouselisteners()
 	{
 		Luxe.core.emitter.on(Luxe.Ev.mousemove, onmousemove);
 		Luxe.core.emitter.on(Luxe.Ev.mouseup, onmouseup);
 	}
-	
-	
+
 	static function removemouselisteners()
 	{
 		Luxe.core.emitter.off(Luxe.Ev.mousemove, onmousemove);
 		Luxe.core.emitter.off(Luxe.Ev.mouseup, onmouseup);
 	}
-	
-	static public function dispose() 
+
+	static public function dispose()
 	{
 		gesturesManager.removeAllGestures();
 		gesturesManager = null;
 		touchesManager = null;
-		
+
 		Luxe.core.emitter.off(Luxe.Ev.touchdown, ontouchdown);
 		Luxe.core.emitter.off(Luxe.Ev.touchmove, ontouchmove);
 		Luxe.core.emitter.off(Luxe.Ev.touchup, ontouchup);
-		
+
 		Luxe.core.emitter.off(Luxe.Ev.mousedown, onmousedown);
 		removemouselisteners();
 	}
